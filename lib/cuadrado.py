3ae86b7b35deafa4160cc1a0900498923351455a
Mariña Vilar Estévez <marina.vilar.estevez@rai.usc.es>
2024-10-08 01:37:11 +0200

    Añadida función para identificarse

--- lib/cuadrado.py
+++ lib/cuadrado.py
@@ -1,5 +1,9 @@
 def get_area(lado):
     return lado*lado
+def get_identificador():
+    return "cuadrado"
 
 resultado=get_area(2)
-print(resultado)
+identificador=get_identificador()
+print("El área es: ",resultado)
+print("El perímetro es: ",perimetro)
