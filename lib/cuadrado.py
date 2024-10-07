d715a080bbb54a23107ea3be49122f82a8e9a278
Mariña Vilar Estévez <marina.vilar.estevez@rai.usc.es>
2024-10-08 01:39:09 +0200

    Añadida función para calcular el perímetro

--- lib/cuadrado.py
+++ lib/cuadrado.py
@@ -2,8 +2,12 @@ def get_area(lado):
     return lado*lado
 def get_identificador():
     return "cuadrado"
+def get_perimetro(lado):
+    return lado*4
 
 resultado=get_area(2)
+perimetro=get_perimetro(2)
 identificador=get_identificador()
 print("El área es: ",resultado)
 print("El perímetro es: ",perimetro)
+print("Es un: ", identificador)
