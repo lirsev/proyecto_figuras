e78cf13baf31ff1a9f513435a90a6e0b7ed4151b
Mariña Vilar Estévez <marina.vilar.estevez@rai.usc.es>
2024-10-08 15:54:36 +0200

    Agregadas funcionalidades para la circunferencia

--- lib/circunferencia.py
+++ lib/circunferencia.py
@@ -1,3 +1,5 @@
 import math
-def get_area(radio:float) -> float:
+def get_area(radio):
     return 2*math.pi*radio
+area=get_area(2)
+print(area)
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from lib import cuadrado, rectangulo, triangulo
+from lib import cuadrado, rectangulo, triangulo, circunferencia
 print("Proyecto Figuras")
 lado=4
 print(f"El área de un {cuadrado.get_identificador()} de lado {lado} es: {cuadrado.get_area(lado)} y el perímetro es {cuadrado.get_perimetro(lado)}")
 
 base=4
 altura=2
+lado_a=2
+lado_b=5
+lado_c=3
 print(f"El área de un {rectangulo.get_identificador()} de base {base} y altura {altura} es: {rectangulo.get_area(base,altura)} y el perímetro es: {rectangulo.get_perimetro(base,altura)}")
 
-base=4
-altura=2
-print(f"El área de un {triangulo.get_identificador()} de base {base} y altura {altura} es: {triangulo.get_area(base,altura)} y el perímetro es {triangulo.get_perimetro(base,altura)}")
+print(f"El área de un {triangulo.get_identificador()} de base {base} y altura {altura} es: {triangulo.get_area(base,altura)} y el perímetro es {triangulo.get_perimetro(lado_a, lado_b, lado_c)}")
 
 
+radio=3
+print(f"El área de la circunferencia de radio {radio} es {circunferencia.get_area(radio)}")
